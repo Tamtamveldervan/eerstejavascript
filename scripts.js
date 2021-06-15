31fc39225ff747f42cd5cfc86ee4cb1fcc9ecb06
Tamtamveldervan <84381450+Tamtamveldervan@users.noreply.github.com>
2021-06-15 13:45:46 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,22 @@
+ console.log("Hello Winc Academy")
+let myName='Tamara'; 
+console.log('Tamara')
+
+let number=4+4; 
+console.log(4+4)
+
+
+let addition=5+9;
+console.log(5+9)
+let substraction=12-8;
+console.log(12-8)
+let multiplication=4*5;
+console.log(4*5)
+let division=8/2;
+console.log(8/2)
+
+
+let age=43;
+console.log(typeof 43)
+
+
